ca60459916fc1c3d63d1f4150acb697bbaa2a1bd
Scantraxx123 <34422209+Scantraxx123@users.noreply.github.com>
2026-02-07 14:47:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "¿Quieres ser mi pareja el 14 de febrero de 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
