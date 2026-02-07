d03037d96bc8200c066d99c279fc79297b244e7d
Felix Willrich <felix.willrich@baader.com>
2026-02-07 15:00:28 +0100

    Update

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dx0ofbbny/video/upload/v1770472580/forrest_gump_08_I_Can_t_Help_Myself_Sugar_Pie_Honey_Bunch_awhraf.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
